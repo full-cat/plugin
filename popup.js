800f02d8c33100f54a2be83399a4ececcf3c79a6
miqalvarez <miquel.alvarez@urv.cat>
2024-11-09 08:29:45 +0100

    Afegir cercat

--- popup.js
+++ popup.js
@@ -1,28 +1,27 @@
 // Recupera el estado del proxy almacenado (si hay alguno)
 chrome.storage.local.get('proxyEnabled', function(result) {
-    if (result.proxyEnabled) {
-      document.getElementById('enableProxy').checked = true;
-    } else {
-      document.getElementById('disableProxy').checked = true;
-    }
-  });
-  
-  // Event listener para cambiar el estado del proxy
-  document.getElementById('enableProxy').addEventListener('change', function() {
-    if (this.checked) {
-      // Activar proxy
-      chrome.storage.local.set({ 'proxyEnabled': true }, function() {
-        chrome.runtime.sendMessage({ action: 'enableProxy' });
-      });
-    }
-  });
-  
-  document.getElementById('disableProxy').addEventListener('change', function() {
-    if (this.checked) {
-      // Desactivar proxy
-      chrome.storage.local.set({ 'proxyEnabled': false }, function() {
-        chrome.runtime.sendMessage({ action: 'disableProxy' });
-      });
-    }
-  });
-  
+  if (result.proxyEnabled) {
+    document.getElementById('enableProxy').checked = true;
+  } else {
+    document.getElementById('disableProxy').checked = true;
+  }
+});
+
+// Event listener para cambiar el estado del proxy
+document.getElementById('enableProxy').addEventListener('change', function() {
+  if (this.checked) {
+    // Activar proxy
+    chrome.storage.local.set({ 'proxyEnabled': true }, function() {
+      chrome.runtime.sendMessage({ action: 'enableProxy' });
+    });
+  }
+});
+
+document.getElementById('disableProxy').addEventListener('change', function() {
+  if (this.checked) {
+    // Desactivar proxy
+    chrome.storage.local.set({ 'proxyEnabled': false }, function() {
+      chrome.runtime.sendMessage({ action: 'disableProxy' });
+    });
+  }
+});
