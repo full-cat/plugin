c7bb67221a23679f916b84f7c196dac975b2fd8e
Enrique Molina <enrique.molina@urv.cat>
2024-11-09 07:57:39 +0100

    plugin

---
+++ popup.js
@@ -0,0 +1,28 @@
+// Recupera el estado del proxy almacenado (si hay alguno)
+chrome.storage.local.get('proxyEnabled', function(result) {
+    if (result.proxyEnabled) {
+      document.getElementById('enableProxy').checked = true;
+    } else {
+      document.getElementById('disableProxy').checked = true;
+    }
+  });
+  
+  // Event listener para cambiar el estado del proxy
+  document.getElementById('enableProxy').addEventListener('change', function() {
+    if (this.checked) {
+      // Activar proxy
+      chrome.storage.local.set({ 'proxyEnabled': true }, function() {
+        chrome.runtime.sendMessage({ action: 'enableProxy' });
+      });
+    }
+  });
+  
+  document.getElementById('disableProxy').addEventListener('change', function() {
+    if (this.checked) {
+      // Desactivar proxy
+      chrome.storage.local.set({ 'proxyEnabled': false }, function() {
+        chrome.runtime.sendMessage({ action: 'disableProxy' });
+      });
+    }
+  });
+  
