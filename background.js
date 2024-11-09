17bdaf2fc32f7aa877a1b9dfd5e2fb7fe21341b3
Enrique Molina <enrique.molina@urv.cat>
2024-11-09 07:18:12 +0100

    radio button

--- background.js
+++ background.js
@@ -1,4 +1,14 @@
 chrome.runtime.onInstalled.addListener(function() {
+  // Si ya está habilitado, configura el proxy al instalar
+  chrome.storage.local.get('proxyEnabled', function(result) {
+    if (result.proxyEnabled) {
+      enableProxy();
+    }
+  });
+});
+
+// Función para habilitar el proxy
+function enableProxy() {
   chrome.proxy.settings.set({
     value: {
       mode: "fixed_servers",
@@ -13,6 +23,27 @@ chrome.runtime.onInstalled.addListener(function() {
     },
     scope: "regular"
   }, function() {
-    console.log("Proxy configurado exitosamente");
+    console.log("Proxy activado");
   });
+}
+
+// Función para deshabilitar el proxy
+function disableProxy() {
+  chrome.proxy.settings.set({
+    value: {
+      mode: "direct"
+    },
+    scope: "regular"
+  }, function() {
+    console.log("Proxy desactivado");
+  });
+}
+
+// Escucha los mensajes desde el popup para activar o desactivar el proxy
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'enableProxy') {
+    enableProxy();
+  } else if (request.action === 'disableProxy') {
+    disableProxy();
+  }
 });
